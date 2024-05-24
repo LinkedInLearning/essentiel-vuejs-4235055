2061a3aa89fd756d32f32664f0883a70820dd90b
LinkedIn User <linkedin@MGR2-BTH107M.linkedin.biz>
2024-05-24 10:37:59 +0200

    01_05

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const app = createApp({
     data() {
         return {
             name: "Pizza Royale",
-            price: 12
+            price: 12,
+            image: "assets/images/pizza1-tomate.jpg"
         }
     }
 })
