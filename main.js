7d9b5210feb59f11a1983e60bb696c303539a8d5
LinkedIn User <linkedin@MGR2-BTH107M.linkedin.biz>
2024-05-24 18:05:27 +0200

    01_08

--- main.js
+++ main.js
@@ -8,9 +8,39 @@ const app = createApp({
             image: "assets/images/pizza1-tomate.jpg",
             classicSale: false,
             superSale: false,
-            notAvailable: true
+            notAvailable: true,
+            ingredients: [
+                "Olives",
+                "Poulet roti",
+                "Bacon",
+                "Poivrons",
+                "Champignons",
+                "Mozzarella",
+                "Oeuf",
+            ],
+            sauces: [
+                {
+                    id: 1001,
+                    type: "Sauce Tomate",
+                    color: "#db4006",
+                    image: "assets/images/pizza1-tomate.jpg",
+                },
+                {
+                    id: 1002,
+                    type: "Crème Fraiche",
+                    color: "#e9cb8f",
+                    image: "assets/images/pizza1-creme.jpg.jpg",
+                },
+            ],
+            energy: {
+                Kcal: 242,
+                Glucides: 27.99,
+                Fibres: 1.75,
+                Proteines: 9.62,
+                Sel: 11,
+            },
         }
     }
 })
 
-app.mount('#app');
+app.mount("#app")
