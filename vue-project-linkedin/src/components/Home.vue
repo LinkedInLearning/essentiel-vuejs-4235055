04ecc67aed520e1a0c1092ae17880d98184c5b4e
LinkedIn User <linkedin@MGR2-BTH107M.linkedin.biz>
2024-05-28 10:19:52 +0200

    05_04_d

--- vue-project-linkedin/src/components/Home.vue
+++ vue-project-linkedin/src/components/Home.vue
@@ -1,15 +1,47 @@
+<template>
+    <div id="intro"></div>
+    <div id="product-menu"></div>
+</template>
+
 <script>
 export default {
     data() {
-        return {}
+        return {};
     },
 
     methods: {},
 
     computed: {}
-}
+    };
 </script>
 
-<template></template>
-
-<style></style>
+<style>
+#intro {
+    background-image: url(../assets/images/pizza-menu.jpg);
+    background-position: bottom;
+    background-size: cover;
+    height: 300px;
+    padding: 3%;
+    width: 100%;
+}
+#intro p {
+    color: white;
+    font-size: 2em;
+    font-weight: bold;
+    text-shadow: black 0.1em 0.1em 0.2em;
+}
+#product-menu {
+    margin-top: 50px;
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    flex-direction: row;
+    flex-wrap: wrap;
+}
+section {
+    display: flex;
+    justify-content: space-around;
+    flex-direction: row;
+    flex-wrap: wrap;
+}
+</style>
