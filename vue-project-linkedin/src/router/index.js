20df89a1a41eb3aea36028086a923b170a0794f4
LinkedIn User <linkedin@MGR2-BTH107M.linkedin.biz>
2024-05-29 15:10:25 +0200

    06_03_d v2

--- vue-project-linkedin/src/router/index.js
+++ vue-project-linkedin/src/router/index.js
@@ -2,11 +2,9 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../view/Home.vue";
 import Contact from "../view/Contact.vue";
 
-const routes = []
-
 const router = createRouter({
     history: createWebHistory(),
-    routes
+    routes: []
 });
 
 export default router;
